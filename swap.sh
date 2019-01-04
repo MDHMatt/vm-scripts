27e35a90dddbc90226db74aae2ce89a8af8c948a
Matt Dinsdale <mattdinsdale@users.noreply.github.com>
2019-01-04 16:19:55 +0000

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -3,9 +3,10 @@
 # Email:        admin@mdhosting.co.uk
 # Date:         2019-01-04
 # Usage:        swap.sh
-# Description:  adds 1gb swap to vm
+# Description:  installs nano wget and screen. adds 1gb swap to vm. sets nano as crontab editor
 # 
 
+yum install -y nano wget screen
 cd /var
 touch swap.img
 chmod 600 swap.img
@@ -13,3 +14,4 @@ dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
 mkswap /var/swap.img
 swapon /var/swap.img
 echo "/var/swap.img    none    swap    sw    0    0" >> /etc/fstab
+export VISUAL=nano; crontab -e
